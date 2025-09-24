adb2e0a9ed0a5c6723f6ae0ac135ca4d8501c59f
Hikari <twilsprk@gmail.com>
2025-09-24 22:13:41 +0100

    hopefully screenshotting pipeline should be ready for later

--- processor_shared/src/lib.rs
+++ processor_shared/src/lib.rs
@@ -1,5 +1,27 @@
+use opencv::{core::{Mat, Vector}, imgcodecs, Error};
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+pub fn convert_image_data(height: u32, width: u32, data: Vec<u8>) -> Result<Mat, Error> {
+    let a = Mat::new_rows_cols_with_data(
+        height as i32, width as i32, &data
+    );
+    match a {
+        Ok(mat) => Ok(mat.clone_pointee()),
+        Err(_) => Err(Error {code: 1, message: "Failed to convert image data".to_string()})
+    }
+}
+
+pub fn save_as_image(mat: Mat) -> Result<(), Error> {
+    if let Ok(_) = imgcodecs::imwrite(&"gray_image_cv2.png", &mat, &Vector::new()) {
+        ()
+    }
+    return Err(opencv::Error {code: 1, message: "Failed to save image data".to_string()})
+
+
+
 }
 
 #[cfg(test)]
--- universal_comms_bot/src/main.rs
+++ universal_comms_bot/src/main.rs
@@ -1,9 +1,9 @@
 use crossbeam::channel::{self, Sender};
 use windows_capture::frame::FrameBuffer;
-use std::thread;
+use std::{process::exit, thread};
 
 mod screenshots;
-
+use processor_shared;
 
 
 fn main() {
@@ -15,8 +15,20 @@ fn main() {
     let (recv, screenshot_controller) = screenshots::capture::spawn_screenshotting_thread();
 
     loop {
-        let frame_data = recv.recv().unwrap();
-      
+        let frame_data = recv.recv().unwrap(); //wait for next frame from recv
+        let Ok(frame) = processor_shared::convert_image_data(
+            frame_data.height,
+            frame_data.width,
+            frame_data.raw_buffer,
+        ) else {
+            // e.g., eprintln!("convert failed");
+            continue;
+        };
+        
+        if let Ok(_) = processor_shared::save_as_image(frame) {
+            exit(0)
+        }
+
     }
 
 }
